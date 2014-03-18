5c471e95ca9cbf7ce774c802ee3d5b647f5c34eb
Jens John <jens.o.john@gmail.com>
2014-03-18 17:28:37 +0900

    document usage (--help flag)

--- apt.lua
+++ apt.lua
@@ -16,6 +16,8 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  This project is being maintained at <https://github.com/2ion/apt-source>.
 --]]
 
 local APT = {}
